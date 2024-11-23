e3d93f369072db05726cbd03a702e7a9e201e698
Aaliyah Haynes <aahaynes@claflin.edu>
2024-11-22 22:10:23 -0500

    New Changes

--- frontend/src/components/homepage.jsx
+++ frontend/src/components/homepage.jsx
@@ -7,7 +7,7 @@ function Homepage() {
         <h2>About</h2>
         <h2>Services</h2>
         <p>Welcome to NutriFit! The Fitness App for you!</p>
-        <h1>Not registed yet? Click heere to sign in</h1>
+        <h1>Not registered yet? Click heere to sign in</h1>
         <button>Sign up</button>
         <h1>Already registered?</h1>
         <button>Login</button>
