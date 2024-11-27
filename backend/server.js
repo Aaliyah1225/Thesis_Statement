fb743d9cc3186a4e9829d4517c47a73d749c0620
Aaliyah <165962589+Geeky99@users.noreply.github.com>
2024-11-27 00:29:03 -0500

    New Changes

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,5 @@
 const mysql = require("mysql2");
 const express = require("express");
-const bodyParser = require("body-parser");
-const bcryptjs = require("bcryptjs");
-
-const app = express();
-const port = 3000;
-
-app.use(bodyParser.json());
 
 // MySQL Connection
 const con = mysql.createConnection({
