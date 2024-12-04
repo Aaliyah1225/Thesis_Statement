facd650940d327fbd1714943837916a826f8d474
Aaliyah <aaliyah_haynes@outlook.com>
2024-12-03 19:19:35 -0500

    New Changes

--- backend/foodserver.js
+++ backend/foodserver.js
@@ -1,37 +1,45 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
 const app = express();
 const port = 3001;
-require('dotenv').config();
+require("dotenv").config();
+const cors = require("cors");
 
-app.use(express.json());
+app.use(cors());
 
-app.post('/nutrition/api', async (req, res) => {
-  const { query } = req.body;
+const APP_ID = process.env.API_ID;
+const APP_KEY = process.env.API_KEY;
+
+app.get("/search", async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: "Query parameter is required." });
+  }
 
   try {
-    const apiID = process.env.NUTRITIONIX_API_ID;
-    const apiKey = process.env.NUTRITIONIX_API_KEY;
-    const response = await fetch.get('https://trackapi.nutritionix.com/v2/natural/nutrients', {
-      method: 'post',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-app-id': apiID,
-        'x-app-key': apiKey,
-      },
-      body: JSON.stringify({ query })
-    });
+    const response = await fetch("https://trackapi.nutritionix.com/v2/natural/nutrients",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+          "x-app-id": APP_ID,
+          "x-app-key": APP_KEY,
+        },
+        body: { query },
+      }
+    );
 
+    if (!response.ok) {
+      throw new error("Failed to fetch data from Nutritionix");
+    }
     const data = await response.json();
-    const nutrients = data.foods.nutrients;
-
-    res.json(nutrients);
+    res.json(data);
   } catch (error) {
-    console.error('Error fetching data:', error);
-    res.status(500).json({ error: 'Failed to fetch nutrition data' });
+    console.error("Error fetching from Nutritionix:", error);
+    res.status(500).json({ error: error.message });
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port http://localhost:${port}`);
+  console.log(`Server listening on port ${port}`);
 });
