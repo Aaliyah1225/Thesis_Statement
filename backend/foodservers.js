0f322ba6bd54ac0dc0a7d138fe272bc2243d4a06
Aaliyah <aaliyah_haynes@outlook.com>
2024-12-04 15:33:19 -0500

    New Changes

--- backend/foodservers.js
+++ backend/foodservers.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 // const port = 3001;
 const axios = require('axios');
-// require("dotenv").config();
+require("dotenv").config();
 const cors = require("cors");
 const router = express.Router();
+const bodyParser = require('body-parser')
 
 app.use(cors());
+app.use(bodyParser.json());
 
 const APP_ID = process.env.API_ID;
 const APP_KEY = process.env.API_KEY;
@@ -19,7 +21,7 @@ const { query } = req.query;
   }
 
   try {
-    const response = await axios.post('https://trackapi.nutritionix.com/v2/natural/nutrients',
+    const response = await axios.post(`https://trackapi.nutritionix.com/v2/natural/nutrients${query}`,
       { query: foodItem },
       {
         headers: {
@@ -29,6 +31,18 @@ const { query } = req.query;
         },
       }
     );
+
+    const foodData = response.data.hits.map((hit) => ({
+      name: hit.fields.item_name,
+      calories: hit.fields.nf_calories,
+      serving_size: hit.fields.nf_serving_size_qty,
+      serving_unit: hit.fields.nf_serving_size_unit,
+      fats: hit.fields.nf_total_fat,
+      protein: hit.fields.nf_protein,
+      carbs: hit.fields.nf_total_carbohydrate,
+      sodium: hit.fields.nf_sodium,
+      sugar: hit.fields.nf_sugars,
+    }));
 
     if (!response.ok) {
       throw new error("Failed to fetch data from Nutritionix");
